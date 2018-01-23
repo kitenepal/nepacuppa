efef83c47d452d3440d102c09e2b32a3e7262be6
NidanStha <nidanshrestha@gmail.com>
2018-01-23 15:19:39 +0545

    nav responsive

---
+++ js/navres.js
@@ -0,0 +1,18 @@
+if (screen.width<=("1000")) {
+    document.getElementsByTagName("nav")[0].setAttribute("data-offset-top", "560");
+} 
+if (screen.width<=("800")) {
+    document.getElementsByTagName("nav")[0].setAttribute("data-offset-top", "400");
+}
+if (screen.width<=("600")) {
+    document.getElementsByTagName("nav")[0].setAttribute("data-offset-top", "340");
+}
+if (screen.width<=("500")) {
+    document.getElementsByTagName("nav")[0].setAttribute("data-offset-top", "280");
+}
+if (screen.width<=("400")) {
+    document.getElementsByTagName("nav")[0].setAttribute("data-offset-top", "230");
+}
+if (screen.width<=("300")) {
+    document.getElementsByTagName("nav")[0].setAttribute("data-offset-top", "170");
+}
